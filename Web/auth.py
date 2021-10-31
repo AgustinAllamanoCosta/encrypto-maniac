20f82c9ba863105d410ba6070feffef202143e6c
AgustinAllamanoCosta <allamanocostaagustin@gmail.com>
2021-10-30 21:06:33 -0300

    Agrega plantilla del sitio con login y un primer modelo del usuario

--- Web/auth.py
+++ Web/auth.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 from Web.aplicacionWeb import aplicacion 
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return 'Login'
+    return render_template('login.html')
 
 @auth.route('/signup')
 def signup():
-    return 'Signup'
+    return render_template('singup.html')
 
 @auth.route('/logout')
 def logout():
-    return 'Logout'
+    return render_template('logout.html')
--- Web/main.py
+++ Web/main.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 from Web.aplicacionWeb import aplicacion 
 
 main = Blueprint('main',__name__)
 
 @main.route('/')
-def indexSitio():
-    return 'Index de flask'
+def index():
+    return render_template('index.html')
 
 @main.route('/profile')
-def perfiles():
-    return 'Perfiles'
+def profile():
+    return render_template('profile.html')
