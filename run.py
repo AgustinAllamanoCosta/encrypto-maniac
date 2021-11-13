48669027dc4dae927d562243469754f4a6631b91
AgustinAllamanoCosta <allamanocostaagustin@gmail.com>
2021-11-13 15:23:50 -0300

    Cambio los script de bash y de shell para que ejecuten la web o la consola de acuerdo a lo que se pasa por parametro, ademas se modifica el script de run.py para que pueda leer los parametros de las consola. Esto es un paso mas en la contruccion del la parte web de la app

--- run.py
+++ run.py
@@ -3,11 +3,11 @@ from Web import aplicacionWeb
 import sys
 
 def run(tipo):
-	if (tipo.lower() == 'consola'):
+	if (tipo[1].lower() == 'consola'):
 		consola = CEM.FactoryConsolaEncriptoManiac().obtenerConsola(sys.platform)
 		consola.bucleDeConsola()
 	else:
 		app = aplicacionWeb.aplicacion()
 		app.crear().run()
 
-run('')
+run(sys.argv)
