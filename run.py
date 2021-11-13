f141c30681d46d4ca85679e514c8c73f9c539bef
AgustinAllamanoCosta <allamanocostaagustin@gmail.com>
2021-11-13 15:35:58 -0300

    Se crean los script para poder levantar la web del encriptador tanto en windows como en linux, ademas se modifica el script de run.py para que soporte no pasarle nada por parametro. Esto es para poder continuar con la contruccion del sitio web del encriptador

--- run.py
+++ run.py
@@ -3,9 +3,10 @@ from Web import aplicacionWeb
 import sys
 
 def run(tipo):
-	if (tipo[1].lower() == 'consola'):
-		consola = CEM.FactoryConsolaEncriptoManiac().obtenerConsola(sys.platform)
-		consola.bucleDeConsola()
+	if(len(tipo) > 1):
+		if (tipo[1].lower() == 'consola'):
+			consola = CEM.FactoryConsolaEncriptoManiac().obtenerConsola(sys.platform)
+			consola.bucleDeConsola()
 	else:
 		app = aplicacionWeb.aplicacion()
 		app.crear().run()
