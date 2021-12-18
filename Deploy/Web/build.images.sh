41a1ea6d6035fe9ba9454f6ea99bac33538fb576
AgustinAllamanoCosta <allamanocostaagustin@gmail.com>
2021-12-18 14:05:19 -0300

    Modifica el nombre de la imagen

--- Deploy/Web/build.images.sh
+++ Deploy/Web/build.images.sh
@@ -1,3 +1,3 @@
 cd $WORKSPACE
-docker build -t ghcr.io/agustinallamanocosta/encryptoManiac:latest -f $1/Dockerfile .
+docker build -t ghcr.io/agustinallamanocosta/encrypto-maniac:latest -f $1/Dockerfile .
 docker push ghcr.io/agustinallamanocosta/encryptoManiac:latest
