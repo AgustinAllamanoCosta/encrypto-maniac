3166a267e149306cea53b4467b49179e3dfa6195
AgustinAllamanoCosta <allamanocostaagustin@gmail.com>
2021-12-18 14:37:34 -0300

    Agrega comando de login para autenticarse en el registri

--- Deploy/Web/build.images.sh
+++ Deploy/Web/build.images.sh
@@ -1,3 +1,4 @@
 cd $WORKSPACE
 docker build -t ghcr.io/agustinallamanocosta/encrypto-maniac:latest -f $1/Dockerfile .
+docker login ghcr.io -u allamanocostaagustin@gmail.com -p $2
 docker push ghcr.io/agustinallamanocosta/encrypto-maniac:latest
