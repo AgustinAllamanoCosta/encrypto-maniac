2a0e517e23e459005343853c834838b05e8ea169
AgustinAllamanoCosta <allamanocostaagustin@gmail.com>
2022-01-27 20:35:12 -0300

    Modifica controladoras de las vista de consultas para que la gestion de las acciones de la mismas pasen por un solo controlador, ademas se agrego el html de los div para los botones de modificar y mostrar contraseñas. Por ultimo se agrego el boton eliminar y su accion

--- Web/EncriptoWeb.py
+++ Web/EncriptoWeb.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from argparse import Action
+import re
 from flask import request
 from flask import Flask
 from flask import render_template
@@ -18,19 +20,20 @@ def login():
 def singUp():
     return render_template('singUp.html')
 
-@aplicacion.route('/consultFrom', methods=['POST','GET'])
+@aplicacion.route('/consultForm', methods=['POST','GET'])
 def consultFrom():
+    if(request.form.get('action') == 'Mostrar'):
+        return encriptador.buscarClave(request.form["nombreCuenta"])
+    elif(request.form.get('action') == 'Eliminar'):
+        encriptador.eliminarClave(request.form.get('nombreCuenta'))
+    elif(request.form.get('action') == 'Agregar'):
+        addCount(request)
+
     return render_template('consult.html',cuentas=buscarCuentas())
 
-@aplicacion.route('/addCount', methods=['POST'])
-def addCount():
+def addCount(request):
     if(request.form["primeraContrasenia"] == request.form["segundaContrasenia"]):
         encriptador.ingresarClave(request.form["nombreCuenta"],request.form["segundaContrasenia"])
-    return render_template('consult.html',cuentas=buscarCuentas())
-
-@aplicacion.route('/acountsFrom', methods=['POST'])
-def acountForm():
-    return encriptador.buscarClave(request.form["nombreCuenta"])
 
 def buscarCuentas():
     cuentasUsuario = []
